293784fd80592a15b722a547cbec11cecb1577d3
Suellen-L <luan.cardoso.0909@gmail.com>
2024-08-09 14:39:51 -0300

    Update delete.py

--- CRUD/delete.py
+++ CRUD/delete.py
@@ -1,10 +1,22 @@
 import sqlite3
 import conexao
 import consultaEstoque
+def deleteCliente():
+    conexao.conectar()
+    global clienteApaga
+    clienteApaga = 
+    try:
+        if(clienteApaga !=[]):
+            conexao.cursor.execute("DELETE FROM cliente WHERE cpf = ?",(clienteApaga))
+            conexao.com.commit()
+            print("cliente removido com sucesso")
+        elif(clienteApaga == []):
+            print("campo não preenchido")
+    except sqlite3.Error as erro:
+        print("erro ",erro)
 
-conexao.conectar()
-global itemApaga
-itemApaga = consultaEstoque
-try:
-    if(itemApaga !=[]):
-        conexao.cursor.execute("DELETE FROM ")
+def deleteVendedor():
+    global vendedorApaga
+
+def deleteProduto():
+    global produtoApaga

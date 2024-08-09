aa3e98436b7fa211ec3424d7bcb00f7ad867ba82
Mariooliveira01 <marioluan0218@gmail.com>
2024-08-09 14:41:25 -0300

    atualização
    09/08 14:41

--- CRUD/criar_tabela.py
+++ CRUD/criar_tabela.py
@@ -1,9 +1,13 @@
 import conexao
 conexao.conectar()
-<<<<<<< HEAD
-def ():
-=======
+
 def tabelaCliente():
     criarTabelaCliente = "CREATE TABLE IF NOT EXISTS cliente(nome string, sobrenome string, cpf integer, telefone integer, rua string, numero integer, cidade string, estado string)"
     conexao.cursor.execute(criarTabelaCliente)
->>>>>>> 56552eacc24a4346294458698dddd70fea0fc26b
+
+def tabelaProduto():
+    criarTabelaProduto = "CREATE TABLE IF NOT EXISTS produto(quantidade integer, tamanho string, valor integer, material string, cor string)"
+    conexao.cursor.execute(criarTabelaProduto)
+
+def tabelaVendedor():
+    criarTabelaVendedor = "CREATE TABLE IF NOT EXISTS vendedor(nome string, sobrenome string, id integer, cpf integer)"

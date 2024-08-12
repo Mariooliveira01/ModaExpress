f705b8f88939a7abe7eb95823588904fd7623de9
Suellen-L <luan.cardoso.0909@gmail.com>
2024-08-12 11:13:23 -0300

    Update registroVendas.py

--- CRUD/registroVendas.py
+++ CRUD/registroVendas.py
@@ -4,6 +4,7 @@ import conexao
 def consultarVenda():
         com = sqlite3.connect('moda_express.db')
         cursor = com.cursor()
+        cursor.execute('SELECT * FROM cliente')
         vendas = cursor.fetchall()
         com.close()
         return vendas

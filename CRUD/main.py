7b5f5c26b7c71abcad78e1b32538018f903d3f15
Suellen-L <luan.cardoso.0909@gmail.com>
2024-09-25 14:12:00 -0300

    um dos erros arrumados
    esta em looping no menu

--- CRUD/main.py
+++ CRUD/main.py
@@ -1,7 +1,7 @@
 import sqlite3
-from funcaoMenu import funcao_menu,menu_cliente,menu_funcionario,menu_estoque
+from funcaoMenu import funcao_menu, menu_cliente, menu_funcionario, menu_estoque
 import conexao
-from criar_tabela import tabelaVendedor,tabelaCliente
+from criar_tabela import tabelaVendedor, tabelaCliente
 import cadastrarFuncionario
 import cadastroCliente
 import cadastroProdutos
@@ -9,44 +9,46 @@ import consultaEstoque
 import registroVendas
 import delete
 import update
+import select
 
-#MENU INICIAL(1- FUNCIONARIO / 2 - CLIENTE)#
+# MENU INICIAL (1 - FUNCIONÁRIO / 2 - CLIENTE) #
 opcao = funcao_menu()
-while (opcao!='3'):
-  if(opcao =='1'):
-    menu_funcionario()
-  elif(opcao =='2'):
-    menu_cliente()
+while opcao != '3':
+    if opcao == '1':
+        menu_funcionario()
+    elif opcao == '2':
+        menu_cliente()
 
+# MENU CLIENTE #
 opcao = menu_cliente()
-while (opcao!='4'):
-  if(opcao =='1'):
-    cadastroCliente.cadastro_cliente()
-  elif(opcao =='2'):
-    update.atualizarCliente()
-  elif(opcao =='3'):
-    delete.deleteCliente()
-   
-#O QUE DESEJA FAZER? PARA FUNCIONARIO#
-opcao = menu_funcionario()
-while (opcao!='5'):
-  if(opcao =='1'):
-    cadastrarFuncionario.cadastro_funcionario()
-  elif(opcao =='2'):
-    update.atualizarVendedor()
-  elif(opcao =='3'):
-    consultaEstoque.menu_estoque()
-  elif(opcao =='4'):
-    delete.deleteVendedor()
+while opcao != '4':
+    if opcao == '1':
+        cadastroCliente.cadastro_cliente()
+    elif opcao == '2':
+        update.atualizarCliente()
+    elif opcao == '3':
+        delete.deleteCliente()
 
-#CASO O FUNCIONARIO  QUEIRA CONSULTAR O ESTOQUE (FUNÇÃO "consulta_estoque")#
+# MENU FUNCIONÁRIO #
+opcao = menu_funcionario()
+while opcao != '5':
+    if opcao == '1':
+        cadastrarFuncionario.cadastro_funcionario()
+    elif opcao == '2':
+        update.atualizarVendedor()
+    elif opcao == '3':
+        consultaEstoque.menu_estoque()
+    elif opcao == '4':
+        delete.deleteVendedor()
+
+# CONSULTA ESTOQUE #
 opcao = menu_estoque()
-while (opcao!='5'):
-  if (opcao == '1'):
-    cadastroProdutos.cadastro_produtos()
-  elif(opcao =='2'):
-    delete.deleteProduto
-  elif(opcao == '3'):
-    registroVendas.vendas
-  elif (opcao =='4'):
-    update.atualizarProduto
+while opcao != '5':
+    if opcao == '1':
+        cadastroProdutos.cadastro_produtos()
+    elif opcao == '2':
+        delete.deleteProduto()
+    elif opcao == '3':
+        registroVendas.vendas()
+    elif opcao == '4':
+        update.atualizarProduto()
